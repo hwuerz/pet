f51d2478042394d0a19cbe100a77ab6b2f8fa1de
h.m.wuerz@web.de <Hendrik Wuerz>
2017-03-08 15:59:04 +0100

    - added variables in cmake command

--- relic/build.sh
+++ relic/build.sh
@@ -20,7 +20,7 @@ export RANLIB="$NDK/$MIDDLE/${PREF}ranlib --sysroot=$SYSROOT"
 export CCLD="$NDK/$MIDDLE/${PREF}gcc --sysroot=$SYSROOT"
 export AR="$NDK/$MIDDLE/${PREF}ar --sysroot=$SYSROOT"
 
-cmake -DWITH="DV;BN;MD;FP;EP;FPX;EPX;PP;PC;CP" -DCHECK=off -DARITH=arm-asm-254 -DARCH=ARM -DCOLOR=off -DOPSYS=DROID -DFP_PRIME=254 -DFP_QNRES=on -DFP_METHD="INTEG;INTEG;INTEG;MONTY;EXGCD;SLIDE" -DFPX_METHD="INTEG;INTEG;LAZYR" -DPP_METHD="LAZYR;OATEP" -DCOMP="-Wall -O3 -funroll-loops -fomit-frame-pointer -I/home/hendrik/Android/Sdk/ndk-bundle/platforms/android-14/arch-arm/usr/include -DuECC_SQUARE_FUNC=1 -DuECC_SUPPORT_COMPRESSED_POINT=0 -DuECC_ENABLE_VLI_API=1 -DuECC_OPTIMIZATION_LEVEL=4 -fPIC" -DLINK="-L/home/hendrik/Android/Sdk/ndk-bundle/platforms/android-14/arch-arm/usr/lib/ -llog -pie" -DTIMER=HREAL -DWORD=32 -DSTLIB=on -DBN_PRECI=3072 -DBENCH=10 -DSHLIB=off $1
+cmake -DWITH="DV;BN;MD;FP;EP;FPX;EPX;PP;PC;CP" -DCHECK=off -DARITH=arm-asm-254 -DARCH=ARM -DCOLOR=off -DOPSYS=DROID -DFP_PRIME=254 -DFP_QNRES=on -DFP_METHD="INTEG;INTEG;INTEG;MONTY;EXGCD;SLIDE" -DFPX_METHD="INTEG;INTEG;LAZYR" -DPP_METHD="LAZYR;OATEP" -DCOMP="-Wall -O3 -funroll-loops -fomit-frame-pointer -I$SYSROOT/usr/include -DuECC_SQUARE_FUNC=1 -DuECC_SUPPORT_COMPRESSED_POINT=0 -DuECC_ENABLE_VLI_API=1 -DuECC_OPTIMIZATION_LEVEL=4 -fPIC" -DLINK="-L$SYSROOT/usr/lib/ -llog -pie" -DTIMER=HREAL -DWORD=32 -DSTLIB=on -DBN_PRECI=3072 -DBENCH=10 -DSHLIB=off $1
 
 make
 adb push bin/bench_cp /data/local/tmp
